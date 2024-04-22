baa7851419ba62c8465f051d2d84e5f021dcab70
doncusspelunkus <marcuspetersen@live.dk>
2024-04-22 16:56:36 +0200

    Base Docker impementation, Merge with Patientservice
    Added featurehub to the patientService branch changes.
    
    Added dockerfiles to the various services.
    
    Added base Xunit project

--- PatientService/Controllers/PatientController.cs
+++ PatientService/Controllers/PatientController.cs
@@ -1,18 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PatientService.Services.Interface;
 
-namespace PatientService.Controllers;
 [ApiController]
 [Route("[controller]")]
 
 public class PatientController : ControllerBase
 {
     private readonly IPatientService _patientService;
-    public PatientController(IPatientService patientService)
-    {
+    private readonly IFeatureHubContext _featureHubContext;
+    public PatientController(IPatientService patientService, IFeatureHubContext featureHubContext){
         _patientService = patientService;
+        _featureHubContext = featureHubContext;
     }
-    
-    
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetPatients()
+    {
+        var featureHubContext = await _featureHubContext.GetFeatureHubContextAsync();
+        return Ok(featureHubContext["Test"].IsEnabled);
+    }
     
 }
---
+++ XunitTest/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace XunitTest;
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
